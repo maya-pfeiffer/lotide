8889b1b9b764df01bc685160637db90d22e38695
Maya Pfeiffer <mayap93@hotmail.com>
2023-06-26 17:10:37 -0400

    Assertion code with emojis

---
+++ assertEqual.js
@@ -0,0 +1,11 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("🎉🎉🎉 Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("😡😡😡 Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Lighthouse Labs");
+assertEqual(6, 1);
