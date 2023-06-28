a5d82a34c012298c37f17c4f33b50aa458d92d5d
Maya Pfeiffer <mayap93@hotmail.com>
2023-06-28 17:47:39 -0400

    Complete countLetters fucntion which definitely didn't make me cry

---
+++ countLetters.js
@@ -0,0 +1,23 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🎉🎉🎉 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`😡😡😡 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(string) {
+  const result = {};
+  for (const letter of string) {
+    if (letter !== " ") {
+    if (result[letter]) {
+      result[letter] += 1;
+    } else {
+      result[letter] = 1;
+    }
+  } 
+}
+  return result;
+};
+
+console.log(countLetters("My name is Maya Pfeiffer"));
