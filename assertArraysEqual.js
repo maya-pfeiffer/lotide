905e84685f9609576dc27836c8046731bda6f5a5
Maya Pfeiffer <mayap93@hotmail.com>
2023-06-29 13:44:49 -0400

    Updates to previous code and complete assertObjectsEqual

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,14 +1,19 @@
-const assertArraysEqual = function(array1, array2) {
+const eqArrays = function(array1, array2) {
   if(array1.length !== array2.length) {
-    console.log(`😡😡😡 Assertion Failed: ${array1} !== ${array2}`);
+    return false;
   } 
   for(let i = 0; i < array1.length; i++) {
     if(array1[i] !== array2[i]) {
-      console.log(`😡😡😡 Assertion Failed: ${array1} !== ${array2}`);
+      return false;
     }
+  }return true;
   }
-  console.log(`🎉🎉🎉 Assertion Passed: ${array1} === ${array2}`);
+
+const assertArraysEqual = function(actual, expected) {
+  if(!eqArrays(actual, expected)) {
+    console.log(`😡😡😡 Assertion Failed: ${actual} !== ${expected}`);
+  } 
+  console.log(`🎉🎉🎉Assertion Passed: ${actual} !== ${expected}`);
   };
 
-  
- console.log(eqArrays([1, 2, 3], [1, 2, 3])); 
+assertArraysEqual([1, 2, 3], [1, 2, 3]); 
---
+++ assertObjectsEqual.js
@@ -0,0 +1,38 @@
+const eqObjects = function(object1, object2) {
+  let keys1 = Object.keys(object1);
+  let keys2 = Object.keys(object2);
+
+  if (keys1.length !== keys2.length) {
+    return false;
+  }
+  for (let key of keys1) {
+    if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
+      if (object1[key].length !== object2[key].length) {
+        return false;
+      }
+      for (let i= 0; i , object1[key]; i++) {
+        if (object1[key][i] !== object2[key][i]) {
+          return false;
+        }
+      }
+    
+  } else {
+  if (object1[key] !== object2[key]) {
+    return false;
+}
+  }
+}
+return true;
+};
+
+const assertObjectsEqual = function(actual, expected) {
+  const inspect = require('util').inspect;
+  if(!eqObjects(actual, expected)) {
+    console.log(`😡😡😡 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
+  } 
+  console.log(`🎉🎉🎉 Assertion Passed: ${inspect(actual)} !== ${inspect(expected)}`);
+}
+
+let result = {colour: "blue", number: 1};
+let answer = {colour: "red", number: 1};
+assertObjectsEqual(result, answer);
