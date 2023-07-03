f6aa5b9c74fb6b99a33a6e8f8c92cc0eeeee3b8a
Maya Pfeiffer <mayap93@hotmail.com>
2023-07-03 15:29:08 -0400

    Refactor tailTest to include Mocha and Chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,6 +1,19 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const tail = require('../tail');
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-console.log(tail(words)); 
-assertEqual(words.length, 3);
+describe("#tail", () => {
+  it("returns [2, 3] for [1, 2, 3]", () => {
+    assert.deepEqual(tail([1, 2, 3]), [2, 3]);
+  });
+  it("returns [55, 78, 333] for [13, 55, 78, 333]", () => {
+    assert.deepEqual(tail([13, 55, 78, 333]), [55, 78, 333]); 
+  });
+});
+
+
+//const assertEqual = require('../assertEqual');
+//const tail = require('../tail');
+
+//const words = ["Yo Yo", "Lighthouse", "Labs"];
+//console.log(tail(words)); 
+//assertEqual(words.length, 3);
